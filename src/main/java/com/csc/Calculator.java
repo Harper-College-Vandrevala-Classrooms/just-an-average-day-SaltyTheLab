feca268842d8541c7c165f114a648c345b23a869
Michael <michaelvargo@outlook.com>
2024-12-06 15:36:09 -0600

    Average day

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,7 +1,74 @@
 package com.csc;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
-  public static void main(String[] args) {
-    System.out.println("This class can house your driver program and / or your functions.");
+  public int minimumfor(int[] numbers) {
+    int smallest = numbers[7];
+    for (int x : numbers)
+      if (x < smallest)
+        smallest = x;
+    return smallest;
   }
+
+  public int maximunfor(int[] numbers) {
+    int max = numbers[3];
+    for (int x : numbers)
+      if (x > max)
+        max = x;
+    return max;
+  }
+
+  public int averagefor(int[] numbers) {
+    int total = 0;
+    for (int x : numbers)
+      total += x;
+    total = total / numbers.length;
+    return total;
+  }
+
+  public int sumfor(int[] numbers) {
+    int sum = 0;
+    for (int x : numbers)
+      sum += x;
+    return sum;
+  }
+
+  public Integer sumstream(List<Integer> numbers) {
+    Integer sum = numbers.stream().reduce(0, (a, b) -> a + b);
+    return sum;
+  }
+
+  public Integer avgstream(List<Integer> numbers) {
+    Integer sum = numbers.stream().reduce(0, (a, b) -> a + b);
+    Integer avg = sum / numbers.size();
+    return avg;
+  }
+
+  public Integer minstream(List<Integer> numbers) {
+    Integer min = numbers.stream().min(Integer::compare).get();
+    return min;
+  }
+
+  public Integer maxstream(List<Integer> numbers) {
+    Integer max = numbers.stream().max(Integer::compare).get();
+    return max;
+  }
+
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    int[] numbersloop = { 7, 1, 3, 0, 4, 6, 8, 5, 9, 2 };
+    List<Integer> numbersstream = Arrays.asList(7, 1, 3, 0, 4, 6, 8, 5, 9, 2);
+    System.out.println(calculator.sumfor(numbersloop));
+    System.out.println(calculator.minimumfor(numbersloop));
+    System.out.println(calculator.maximunfor(numbersloop));
+    System.out.println(calculator.averagefor(numbersloop));
+    System.out.println(calculator.sumstream(numbersstream));
+    System.out.println(calculator.minstream(numbersstream));
+    System.out.println(calculator.maxstream(numbersstream));
+    System.out.println(calculator.avgstream(numbersstream));
+
+  }
+
 }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -1,12 +1,15 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCalculator {
-
+  int[] numbersloop = { 7, 1, 3, 0, 4, 6, 8, 5, 9, 2 };
+  List<Integer> numbersstream = Arrays.asList(4, 7, 6, 2, 1, 3, 8, 9, 0, 5);
   Calculator calculator;
 
   @BeforeEach
@@ -15,7 +18,42 @@ public class TestCalculator {
   }
 
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void sumtest() {
+    assertEquals(45, calculator.sumfor(numbersloop));
+  }
+
+  @Test
+  void avgtest() {
+    assertEquals(4, calculator.averagefor(numbersloop));
+  }
+
+  @Test
+  void maxfor() {
+    assertEquals(9, calculator.maximunfor(numbersloop));
+  }
+
+  @Test
+  void minfor() {
+    assertEquals(0, calculator.minimumfor(numbersloop));
+  }
+
+  @Test
+  void sumstream() {
+    assertEquals(45, calculator.sumstream(numbersstream));
+  }
+
+  @Test
+  void minstream() {
+    assertEquals(0, calculator.minstream(numbersstream));
+  }
+
+  @Test
+  void maxstream() {
+    assertEquals(9, calculator.maxstream(numbersstream));
+  }
+
+  @Test
+  void avgstream() {
+    assertEquals(4, calculator.avgstream(numbersstream));
   }
 }
